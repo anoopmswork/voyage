6c536290767576ac1fcfbbdde04c2860a140d13d
anoopms1988 <1988anoopms@gmail.com>
2018-04-20 06:55:09 +0530

    Voyage settings details

---
+++ voyage/settings/constants.py
@@ -0,0 +1,20 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ')vc@b)ffcrzhc3gp*59=cxom!8x8es!gki@7kzu%^e1nx*$@_6'
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_URL = '/static/'
