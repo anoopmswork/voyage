43cc905589109ad37a9f2d44e779b959d01ae0f8
anoopms1988 <1988anoopms@gmail.com>
2018-04-21 13:13:24 +0530

    Assets and grapelli

--- voyage/settings/constants.py
+++ voyage/settings/constants.py
@@ -1,3 +1,7 @@
+# noinspection PyPackageRequirements
+import os
+import datetime
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ')vc@b)ffcrzhc3gp*59=cxom!8x8es!gki@7kzu%^e1nx*$@_6'
 # Internationalization
@@ -17,4 +21,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT_DIR = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '../../'))
+STATIC_ROOT_DIR = os.environ.get('STATIC_ROOT_DIR', PROJECT_ROOT_DIR)
+STATIC_URL = '/assets/static/'
+
+STATIC_ROOT = os.path.join(STATIC_ROOT_DIR, 'assets', 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(STATIC_ROOT_DIR, 'assets', 'media')
+
 STATIC_URL = '/static/'
+
+GRAPPELLI_INDEX_DASHBOARD ='voyage.dashboard.CustomIndexDashboard'
+
+GRAPPELLI_ADMIN_TITLE = "Voyage"
--- voyage/settings/installed_apps.py
+++ voyage/settings/installed_apps.py
@@ -1,11 +1,22 @@
 # Application definition
 
-INSTALLED_APPS = (
+DJANGO_APPS = (
+    'django.contrib.contenttypes',
+    'grappelli.dashboard',
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+)
+
+THIRD_PARTY_APPS = (
     'rest_framework',
 )
+
+LOCAL_APPS = (
+
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
