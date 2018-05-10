34f71f682722a0fa8004f11a333e6cf99b437348
anoopms1988 <1988anoopms@gmail.com>
2018-05-11 05:05:53 +0530

    Signals and receivers configuration

--- apps/account/apps.py
+++ apps/account/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from django.dispatch import Signal
+
 
 
 class AccountConfig(AppConfig):
     name = 'apps.account'
+
+    def ready(self):
+        from . import receivers
---
+++ apps/account/receivers.py
@@ -0,0 +1,72 @@
+import logging
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from core import errors as err
+from .models import UserProfile, AuditEntry
+from .signals import *
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    try:
+        if created:
+            UserProfile.objects.create(user=instance)
+    except Exception as e:
+        logger.error(e)
+        raise err.ValidationError(*(e, 400))
+
+
+@receiver(user_logged_in)
+def user_logged_in_callback(sender, request, user, **kwargs):
+    """
+    Track user login details
+    :param sender:
+    :param request:
+    :param user:
+    :param kwargs:
+    :return:
+    """
+    try:
+        ip = request.META.get('REMOTE_ADDR')
+        AuditEntry.objects.create(action='user_logged_in', ip=ip, username=user.username)
+    except Exception as e:
+        logger.error(e)
+        raise err.ValidationError(*(e, 400))
+
+
+@receiver(user_logged_out)
+def user_logged_out_callback(sender, request, user, **kwargs):
+    """
+    Track user logged out details
+    :param sender:
+    :param request:
+    :param user:
+    :param kwargs:
+    :return:
+    """
+    try:
+        ip = request.META.get('REMOTE_ADDR')
+        AuditEntry.objects.create(action='user_logged_out', ip=ip, username=user.username)
+    except Exception as e:
+        logger.error(e)
+        raise err.ValidationError(*(e, 400))
+
+
+@receiver(user_login_failed)
+def user_login_failed_callback(sender, credentials, **kwargs):
+    """
+    Track user logged out details
+    :param sender:
+    :param credentials:
+    :param kwargs:
+    :return:
+    """
+    try:
+        AuditEntry.objects.create(action='user_login_failed', username=credentials.get('username', None))
+    except Exception as e:
+        logger.error(e)
+        raise err.ValidationError(*(e, 400))
