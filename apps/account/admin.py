28be763082995916682f7ffd43053f4b5eb936d3
anoopms1988 <1988anoopms@gmail.com>
2018-05-10 06:08:24 +0530

    Audit admin

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AuditEntry
+
+@admin.register(AuditEntry)
+class AuditEntryAdmin(admin.ModelAdmin):
+    list_display = ['action', 'username', 'ip',]
+    list_filter = ['action',]
