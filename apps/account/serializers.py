abe9512354abd91c9b707e811d3f4b0e3bcdd79c
anoopms1988 <1988anoopms@gmail.com>
2018-05-16 05:29:55 +0530

    login atemppts

--- apps/account/serializers.py
+++ apps/account/serializers.py
@@ -1,7 +1,15 @@
 from core.serializers import ExModelSerializer
 from django.contrib.auth.models import User
+from .models import AuditEntry
+
 
 class UserSerializer(ExModelSerializer):
     class Meta:
         model = User
+        exclude = ()
+
+
+class AuditEntrySerializer(ExModelSerializer):
+    class Meta:
+        model = AuditEntry
         exclude = ()
